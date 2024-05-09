781c35c471d88bf1cafc19f89b74896ed4668173
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-05-09 15:39:32 +0300

    Added the correct changes

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { actors } from "../data";
-
+// allActors=actors.map((actor,index)=>(
+//   <div key={index}>
+//     <h1>{actor.name}</h1>
+//     {actor.movies.map((movie,index)=>(
+//       <ul key={index}> {movie}</ul>
+//     ))}
+//   </div>
+// ))
 function Actors() {
-  const allActors=actors.map((actor,index)=>(
-    <div key={index}>
-      <h1>{actor.name}</h1>
-      {actor.movies.map((movie,index)=>(
-        <ul key={index}> {movie}</ul>
-      ))}
+  const  allActors=actors.map((actor,index)=>(
+   <div key={index}>
+    <h1>{actor.name}</h1>
+    {actor.movies.map((movie,index)=>(
+      <li key={index}>{movie}</li>
+    ))}
     </div>
+   
   ))
   return (<div>
     <h1>Actors Page</h1>
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,45 +1,22 @@
 import React from "react";
 import { directors } from "../data";
 
-function Directors() {
+function Directors(){
   const allDirectors=directors.map((director,index)=>(
     <div key={index}>
       <h1>{director.name}</h1>
-      {director.movies.map((aMovie,index)=>(
-        <li key={index}>{aMovie}</li>
+      {director.movies.map((movie,index)=>(
+        <li key={index}>{movie}</li>
       ))}
-    </div>
-  ))
-
+    </div>    
+  ))  
   return (
+    
     <div>
       <h1>Directors Page</h1>
       {allDirectors}
     </div>   
        
-  )
-
-
-
-
-
-
-
-
-
-  //const allDirectors=directors.map((director,index)=>(
-  //   <div key={index}style={{margin:10}}>
-  //     <h1>{director.name}</h1>
-  //   {director.movies.map((movie,index)=>(
-  //     <li key={index}>{movie}</li>
-  //   ))}
-  //   </div>
-  // ))
-  // return( <div>
-  //   <h1>Directors Page</h1>
-  //   {allDirectors}
-  // </div>
-  // );
+  )  
 }
-
 export default Directors;
